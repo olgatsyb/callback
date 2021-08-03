f517df24c38babf27073e4b9e344a41fb053093e
Olga <helgatsyb@gmail.com>
2021-08-03 10:22:14 +0300

    first check

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@ function handleSubmit(evt) {
     evt.preventDefault();
 
     const phone = phoneInputEl.value.trim();
-
     const data = {
         phone,
     };
 
+    messageEl.textContent = '';
+    phoneErrorEl.textContent = '';
+    phoneInputEl.textContent = '';
+
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:9999/api/lection/cards');
+    xhr.open('POST', 'http://localhost:9999/api/hw14');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = handleSuccess;
     xhr.onerror = handleError;
@@ -19,20 +22,27 @@ function handleSubmit(evt) {
 
 function handleSuccess (evt) {
     if (evt.target.status !== 200){
-        // TODO: handle error
+        const data = JSON.parse(evt.target.responseText);
+        phoneErrorEl.textContent = data.error;
+        messageEl.textContent = 'Произошла ошибка';
         return;
     }
 
     const data = JSON.parse(evt.target.responseText);
-    // TODO: work with data
+    messageEl.textContent = 'Успешно отправлено! Наш менеджер перезвонит в течение 15 минут.';
 }
 
 function handleError (evt){
-    // TODO: handle error
+    if (evt.target.status === 400){
+        const data = JSON.parse(evt.target.responseText);
+        phoneErrorEl.textContent = data.error;
+        messageEl.textContent = 'Произошла ошибка';
+    }
 }
 
-const formEl = document.getElementById('card-form');
+const formEl = document.getElementById('callback-form');
 formEl.addEventListener('submit', handleSubmit);
 
+const messageEl = document.getElementById('message');
 const phoneInputEl = document.getElementById('phone-input');
 const phoneErrorEl = document.getElementById('phone-error');
